f212d35c00e0f46f3f93ff693e18f17d9f580fca
Minoru Nakano <minoru.nakano@gmail.com>
2021-12-23 16:30:45 -0500

    removed R script

--- download.R
+++
@@ -1,9 +0,0 @@
-require(downloader)
-library(curl)
-
-options(stringsAsFactors = FALSE)
-
-out_dir <- "/pfs/out/"
-
-curl_download("https://figshare.com/ndownloader/articles/2069274/versions/16", destfile = paste0(out_dir, "16"))
-unzip(paste0(out_dir, "16"), exdir=out_dir)
